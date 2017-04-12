ba3bcfa01137f31181de8458400a4c6f87ec295b
Andrei-Khadziukou <Andrei_Khadziukou@epam.com>
2017-04-12 17:31:52 +0300

    Configuration to connect to Eureka was added

--- src/main/java/com/epam/petclinic/clinic/PetClinicClinicApplication.java
+++ src/main/java/com/epam/petclinic/clinic/PetClinicClinicApplication.java
@@ -2,11 +2,13 @@ package com.epam.petclinic.clinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PetClinicClinicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetClinicClinicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicClinicApplication.class, args);
+    }
 }
