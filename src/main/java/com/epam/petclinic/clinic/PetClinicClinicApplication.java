3b97583b6363ded60d710d82d4e9ef4db4fa7f76
Andrei-Khadziukou <khadziukou_andrei@bk.ru>
2017-04-12 15:48:49 +0300

    Initiate repository

---
+++ src/main/java/com/epam/petclinic/clinic/PetClinicClinicApplication.java
@@ -0,0 +1,12 @@
+package com.epam.petclinic.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicClinicApplication.class, args);
+	}
+}
